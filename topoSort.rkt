42cb5c843dd8496933a12e4bb4d4592ed8bc76eb
Mark Anderson <Mark.Anderson.IX@Gmail.com>
2015-03-13 19:43:32 -0700

    WIP

--- topoSort.rkt
+++ topoSort.rkt
@@ -12,10 +12,48 @@
 ;    return error (graph has at least one cycle)
 ;else 
 ;    return L (a topologically sorted order)
-(define (topoSort a)
-    (if (empty? a #f) ;base case
-        (cons (caar a)) ((topoSort (cdar a) )      ;recursive case which is not done
-  
-  
-  )
 
+  
+;(define (topoSort a)
+;    (if (empty? a #f) ;base case
+;    (cons (caar a)) ((topoSort (cdar a) )      ;recursive case which is not done
+  
+;)))
+
+;check if element a list L contains an element
+(define (contains a L)
+  (if (eqv? (car L) a) #t
+     (if(null? (cdr L)) #f (contains (cdr L) a))
+   )
+)
+
+;check if vertex a in graph G has indegree greater than 0
+(define (indegree a G)
+  (if(contains  a (car G)) #t
+     (if(null? (cdr G)) #f (contains a (cdr G)))
+    )
+)
+;remove an element from G
+(define (remove a G)
+  (if (null? G)
+      '()
+      (if (eqv? a (car G))
+          (remove a (cdr G))
+          (cons (car G)
+                (remove a (cdr G))))))
+
+;get next vertex to remove
+(define (getnext G)
+  (if(null? G) null ;reached end -> cycle
+     (if (indegree car(car G)) ;if head of list has no indegree, return it
+         (getnext(cdr G))
+         (car(car G)))
+))
+
+;topoSort function
+(define (topoSort G)
+  (if(empty? G) null ;base case
+      (if (eqv? (getnext G) null) "Has at least one cycle." ;cyclic
+               (cons (getnext G) topoSort(remove (getnext G)))
+  ))
+)
